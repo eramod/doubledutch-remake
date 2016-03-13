61d42243165c1ecf3e05a621cad79841713f0505
eramod <dianne.eramo@gmail.com>
2016-03-13 19:32:15 -0400

    add options for slider and change some css to make it work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	auto: true,
+  	controls: false,
+  	minSlides: 1,
+  	maxSlides: 1,
+  	pager: false,
+  	slideWidth: 960
+  });
+});
